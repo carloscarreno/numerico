22e7f151c635ed0356763397175a2518cb8a47ad
carloscarreno <carlos.carreno.pe@gmail.com>
2025-08-10 23:53:03 -0500

    update 3

--- src/main/java/org/jbossgroup/numerico/controller/NumericoController.java
+++ src/main/java/org/jbossgroup/numerico/controller/NumericoController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.jbossgroup.numerico.service.NumericoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,26 @@ public class NumericoController {
 	@Autowired
 	private NumericoService numericoService;
 	
+	// Cualquier usuario puede ver el mensaje de bienvenida
+	@GetMapping("/welcome")
+    public String welcome() {
+       return "Bienvenidos todos al Generador de Numeros Primos! ";
+    }
+
+	// Solo los usuarios autorizados puedes realizar la operacion
 	@GetMapping(path="/primos/{n}",produces=MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasRole('user_client') or hasRole('admin_client')")
 	public  Map<String, Object> getPrimos(@PathVariable Long n){
 		Map<String, Object> map = new HashMap<>();
 	    map.put("primos", numericoService.getPrimos(n));
 	    return map;		
 	}
 	
+	// Solo un administrador puede ver los creditos
+	@GetMapping("/creditos")
+    @PreAuthorize("hasRole('admin_client')") 
+    public String getCreditos() {
+        return "Desarrollados por ccarrenovi@gmail.com - ONLY ADMIN";
+    }
+
 }
